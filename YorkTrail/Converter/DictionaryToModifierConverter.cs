cde96c4b4b263cbf7414f96d3b44a52213d54db7
theta88p <theta88p@gmail.com>
2021-01-28 23:43:21 +0900

    ショートカットを可変にできるようにする下準備 コンバータをフォルダにまとめた

---
+++ YorkTrail/Converter/DictionaryToModifierConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace YorkTrail.Converter
+{
+    class DictionaryToModifierConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType == typeof(Key))
+            {
+                var dic = value as Dictionary<string, KeyGesture>;
+                KeyGesture kg;
+
+                if (dic.TryGetValue(parameter as string, out kg))
+                {
+                    if (targetType == typeof(Key))
+                    {
+                        return kg.Key;
+                    }
+                    else if (targetType == typeof(ModifierKeys))
+                    {
+                        return kg.Modifiers;
+                    }
+                }
+            }
+
+            if (targetType == typeof(Key))
+            {
+                return Key.None;
+            }
+            else if (targetType == typeof(ModifierKeys))
+            {
+                return ModifierKeys.None;
+            }
+            return Key.None;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- YorkTrail/Settings.cs
+++ YorkTrail/Settings.cs
@@ -5,28 +5,50 @@ using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace YorkTrail
 {
+    [DataContract]
     public class Settings : INotifyPropertyChanged
     {
         public Settings()
-        {}
-
+        {
+            SetDefaultKeyBinds();
+        }
+        [DataMember]
         public float Volume { get; set; } = 1.0f;
+        [DataMember]
         public int DeviceIndex { get; set; } = 0;
+        [DataMember]
         public string DeviceName { get; set; }
+        [DataMember]
         public ObservableCollection<string> RecentFiles { get; set; } = new ObservableCollection<string>();
+        [DataMember]
         public double WindowHeight { get; set; }
+        [DataMember]
         public int MenuToolBarBand { get; set; }
+        [DataMember]
         public int MenuToolBarBandIndex { get; set; }
+        [DataMember]
         public double MenuToolBarWidth { get; set; }
+        [DataMember]
         public int IconToolBarBand { get; set; }
+        [DataMember]
         public int IconToolBarBandIndex { get; set; }
+        [DataMember]
         public double IconToolBarWidth { get; set; }
+        public Dictionary<string, KeyGesture> KeyBinds { get; set; }
+        [DataMember]
+        public Dictionary<string, string> keyBindsSerializeable;
+
+        [DataMember]
         private bool _alwaysOnTop = false;
         public bool AlwaysOnTop {
             get { return _alwaysOnTop; }
@@ -42,19 +64,44 @@ namespace YorkTrail
         private const string xmlPath = @".\Settings.xml";
         public static Settings ReadSettingsFromFile()
         {
-            Settings settings;
+            Settings settings = null;
             if (File.Exists(xmlPath))
             {
                 // デシリアライズする
-                var xmlSerializer = new XmlSerializer(typeof(Settings));
-                var xmlSettings = new System.Xml.XmlReaderSettings()
+                var serializer = new DataContractSerializer(typeof(Settings));
+                var xmlSettings = new XmlReaderSettings()
                 {
                     CheckCharacters = false,
                 };
-                using (var streamReader = new StreamReader(xmlPath, Encoding.UTF8))
-                using (var xmlReader = System.Xml.XmlReader.Create(streamReader, xmlSettings))
+
+                try
                 {
-                    settings = (Settings)xmlSerializer.Deserialize(xmlReader);
+                    using (var streamReader = new StreamReader(xmlPath, Encoding.UTF8))
+                    using (var xmlReader = XmlReader.Create(streamReader, xmlSettings))
+                    {
+                        settings = (Settings)serializer.ReadObject(xmlReader);
+                    }
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show("設定ファイルの読み込みに失敗しました\n\n" + e.Message, "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+                if (settings != null)
+                {
+                    var kgvs = new KeyGestureValueSerializer();
+                    settings.SetDefaultKeyBinds();
+
+                    foreach (var kb in settings.keyBindsSerializeable)
+                    {
+                        if (kgvs.CanConvertFromString(kb.Value, null))
+                        {
+                            if (settings.KeyBinds.ContainsKey(kb.Key))
+                            {
+                                settings.KeyBinds[kb.Key] = (KeyGesture)kgvs.ConvertFromString(kb.Value, null);
+                            }
+                        }
+                    }
                 }
             }
             else
@@ -66,15 +113,50 @@ namespace YorkTrail
 
         public static void WriteSettingsToFile(Settings settings)
         {
-            // シリアライズする
-            var xmlSerializer1 = new XmlSerializer(typeof(Settings));
-            using (var streamWriter = new StreamWriter(xmlPath, false, Encoding.UTF8))
+            var kgvs = new KeyGestureValueSerializer();
+            settings.keyBindsSerializeable = new Dictionary<string, string>();
+
+            foreach(var kb in settings.KeyBinds)
             {
-                xmlSerializer1.Serialize(streamWriter, settings);
-                streamWriter.Flush();
+                if (kgvs.CanConvertToString(kb.Value, null))
+                {
+                    settings.keyBindsSerializeable.Add(kb.Key, kgvs.ConvertToString(kb.Value, null));
+                }
+            }
+            // シリアライズする
+            var serializer = new DataContractSerializer(typeof(Settings));
+            var set = new XmlWriterSettings();
+            set.CheckCharacters = false;
+            set.Encoding = Encoding.UTF8;
+            set.Indent = true;
+
+            try
+            {
+                using (var xmlWriter = XmlWriter.Create(xmlPath, set))
+                {
+                    serializer.WriteObject(xmlWriter, settings);
+                    xmlWriter.Flush();
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("設定ファイルの書き込みに失敗しました\n\n" + e.Message, "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
+        private void SetDefaultKeyBinds()
+        {
+            var dic = new Dictionary<string, KeyGesture>() {
+                { "Play", new KeyGesture(Key.Space, ModifierKeys.None) },
+                { "Pause", new KeyGesture(Key.Enter, ModifierKeys.None) },
+                { "Stop", new KeyGesture(Key.Escape, ModifierKeys.None) },
+                { "FF", new KeyGesture(Key.Right, ModifierKeys.None) },
+                { "FR", new KeyGesture(Key.Left, ModifierKeys.None) },
+                { "ToStart", new KeyGesture(Key.Left, ModifierKeys.Shift) },
+                { "ToEnd", new KeyGesture(Key.Right, ModifierKeys.Shift) }
+            };
 
+            this.KeyBinds = dic;
+        }
     }
 }
